2eeb3efca6e29d34a91e711ed2ade7fe176de542
Pramod Gupta <P10484337@capita.co.uk>
2021-03-03 12:07:52 +0530

    added custom button for signin

---
+++ src/components/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,11 @@
+
+import SignIn from '../../components/sign-in/sign-in.component';
+import './sign-in-and-sign-up.styles.scss';
+
+const SigninAndSignUpPage = () =>  (
+    <div className='sign-in-and-sign-up'>
+        <SignIn />
+    </div>
+)
+
+export default SigninAndSignUpPage;
