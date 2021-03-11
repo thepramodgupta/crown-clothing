3e14a628293e3f6c4be675ba357fb218b083d21a
Pramod Gupta <P10484337@capita.co.uk>
2021-03-11 22:01:11 +0530

    added cartIcon and CartDropdown component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,10 +2,12 @@ import { Link } from 'react-router-dom';
 import { auth } from '../../firebase/firebase.utils';
 import { connect } from 'react-redux';
 
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import { ReactComponent as Logo } from '../../assets/crown.svg'
 import './header.styles.scss';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
     <div className="header">
         <Link className='logo-container' to="/">
             <Logo className='Logo' />
@@ -28,12 +30,22 @@ const Header = ({ currentUser }) => (
                 SIGN IN
                 </Link>
             }
+            <CartIcon />
         </div>
+        {
+            hidden?
+                null
+            :<CartDropdown />
+
+        }
     </div>
 )
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+// this is setting props from state value, it's not triggering any action, 
+//from here it goes to up in component as props
+const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({
+    currentUser,
+    hidden
 })
 
 export default connect(mapStateToProps)(Header);
