341a685081a1703d4599fc7b6f65440bb0bb97dc
Pramod Gupta <P10484337@capita.co.uk>
2021-03-22 20:56:56 +0530

    added add item and remove item functionality to checkout

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 import './cart-item.styles.scss';
 
-const CartItem = ({ item: {imageUrl, price, name, quanity} }) => {
+const CartItem = ({ item: {imageUrl, price, name, quantity} }) => {
     console.log('called')
     return (
     <div className="cart-item">
         <img src={imageUrl} alt="item"/>
         <div className="item-details">
             <span className="name">{name}</span>
-            <span className="price">{quanity} x ${price}</span>
+            <span className="price">{quantity} x ${price}</span>
         </div>
     </div>
 )}
--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -15,11 +15,11 @@ export const selectCartHidden = createSelector(
 export const selectCartItemsCount = createSelector(
     [selectCartItems],
     cartItems => cartItems.reduce(
-        (accumulatedQuanity, cartItem) => accumulatedQuanity + cartItem.quanity, 0)
+        (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 0)
 );
 
 export const selectCartTotal = createSelector(
     [selectCartItems],
     cartItems => cartItems.reduce(
-        (accumulatedQuanity, cartItem) => accumulatedQuanity + cartItem.quanity * cartItem.price, 0)
+        (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity * cartItem.price, 0)
 );
