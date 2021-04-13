6e432b0994f129dff27be6b4e67134e18996dc23
Pramod Gupta <P10484337@capita.co.uk>
2021-04-13 17:53:49 +0530

    Added With with-spinner HOC and changed shop component firestore data call from observable to promise pattern

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -7,29 +7,39 @@ import CollectionsPage from '../collections/collections.component';
 
 import { updateCollections } from "../../redux/shop/shop.actions";
 
+import WithSpinner from '../../components/with-spinner/with-spinner.component';
+
 import { firestore, convertCollectionSnapshotToMap } from '../../firebase/firebase.utils';
 
+const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
+const CollectionPageWithSpinner = WithSpinner(CollectionsPage);
 
 class ShopPage extends Component { 
+  state = {
+    loading: true
+  };
 
   unsubscribeFromSnapShot = null;
 
   componentDidMount() {
     const { updateCollections } = this.props;
     const collectionRef = firestore.collection('collections');
-    collectionRef.onSnapshot(async snapshot => {
+
+    collectionRef.get().then(snapshot => {
       const collectionsMap = convertCollectionSnapshotToMap(snapshot);
       console.log(collectionsMap);
       updateCollections(collectionsMap);
-    })
+      this.setState({ loading:false });
+    });
 
   }
   render() {
     const { match } = this.props;
+    const { loading } = this.state;
     return (
       <div className='shop-page'>
-        <Route exact path={`${match.path}`} component={CollectionsOverview} />
-        <Route path={`${match.path}/:collectionId`} component={CollectionsPage} />
+        <Route exact path={`${match.path}`} render={(props)=><CollectionsOverviewWithSpinner isLoading={loading} {...props} /> } />
+        <Route path={`${match.path}/:collectionId`} render={(props)=><CollectionPageWithSpinner isLoading={loading} {...props}/>} />
       </div>
     )
   }
