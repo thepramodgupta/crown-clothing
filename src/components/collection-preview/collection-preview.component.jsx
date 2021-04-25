0705171195fe155098bbd32d24122091fb27bbb3
Pramod Gupta <P10484337@capita.co.uk>
2021-04-25 10:56:54 +0530

    added HOC in collection-item and collection-preview

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,25 +1,27 @@
-import CollectionItem from '../collection-item/collection-item.component'
+import React from 'react';
+import { withRouter } from 'react-router-dom';
 
-import './collection-preview.styles.scss';
+import CollectionItem from '../collection-item/collection-item.component';
 
-const CollectionPreview = ({ title, items }) => (
-    <div className='collection-preview'>
-        <h1 className='title'> { title.toUpperCase() } </h1>
-        <div className='preview'>
-            {
-                items
-                .filter((item, idx) => idx < 4)
-                .map(
-                        (item) => (
-                            <CollectionItem key={item.id} item={item} />
-                        )
-                )
-            }
+import {
+  CollectionPreviewContainer,
+  TitleContainer,
+  PreviewContainer
+} from './collection-preview.styles';
 
-           
-        </div>
-    </div>
+const CollectionPreview = ({ title, items, history, match, routeName }) => (
+  <CollectionPreviewContainer>
+    <TitleContainer onClick={() => history.push(`${match.path}/${routeName}`)}>
+      {title.toUpperCase()}
+    </TitleContainer>
+    <PreviewContainer>
+      {items
+        .filter((item, idx) => idx < 4)
+        .map(item => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+    </PreviewContainer>
+  </CollectionPreviewContainer>
 );
 
-export default CollectionPreview;
-
+export default withRouter(CollectionPreview);
